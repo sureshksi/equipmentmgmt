18e1e45d2aec6df68e82abad5e19dc5892b43912
Suresh Injeti <sureshkumar.injeti@hcl.com>
2025-11-01 20:57:09 +0530

    borrowed column added

--- mysql/init.sql
+++ mysql/init.sql
@@ -10,6 +10,7 @@ CREATE TABLE `equipment` (
   `quantity` int DEFAULT NULL,
   `available` tinyint NOT NULL DEFAULT '0',
   `createdon` datetime NOT NULL,
+  `borrowed` int DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
