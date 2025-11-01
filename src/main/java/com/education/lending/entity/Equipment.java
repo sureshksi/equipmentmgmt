24d196560d9e812b1d8020ee6314c11819d1c08c
Suresh Injeti <sureshkumar.injeti@hcl.com>
2025-11-01 20:40:46 +0530

    Default set 0 for borrowed

--- src/main/java/com/education/lending/entity/Equipment.java
+++ src/main/java/com/education/lending/entity/Equipment.java
@@ -26,6 +26,6 @@ public class Equipment {
     private String conditiontype;
     private Integer quantity;
     private Boolean available;
-    private Integer borrowed;
+    private Integer borrowed=0;
 }
 
